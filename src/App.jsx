adc29f11be1249569b8aac668ba3170a6b42f4d7
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-02 23:11:17 +0500

    intail commit multiStepInput setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MultiStep from './Component/MultiStep'
+
+const App = () => {
+  return (
+    <>
+    <MultiStep />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/MultiStep.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MultiStep = () => {
+
+  return (
+    <div>
+    <form action="">
+        <label htmlFor=""></label>
+        <input type="text" name="" id="" placeholder='Enter Your name' />
+        <button>Next</button>
+    </form>
+    </div>
+  )
+}
+
+export default MultiStep
