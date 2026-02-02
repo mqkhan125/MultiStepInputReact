0825c169f880bb2d1b64326b487c8ec92498cb69
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-02 23:44:05 +0500

    Add multistep form logic and styling

--- src/Component/MultiStep.jsx
+++ src/Component/MultiStep.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 
 const MultiStep = () => {
 
+    const data = [
+
+        {
+           id: 'name',
+           buttonName: 'Next',
+           placeholder: 'Enter your name',
+           inputText: 'text',
+           label: 'Name',
+       },
+        {
+           id: 'email',
+           buttonName: 'Next',
+           placeholder: 'Enter your email',
+           inputText: 'email',
+           label: 'Email',
+       },
+       {
+           id: 'password',
+           buttonName: 'Next',
+           placeholder: 'Enter your password',
+           inputText: 'password',
+           label: 'Password'
+       },
+       {
+          id: 'dob',
+          buttonName: 'Submit',
+          placeholder: 'Enter your dob',
+          inputText: 'date',
+          label: 'DOB'
+      }
+    ]
+
   return (
     <div>
     <form action="">
